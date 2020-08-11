cb97d9d7ec57045999c3970b41d99c1703b4a260
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 14:35:47 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
-  
+  attr_accessor :name, :age
+@@all = []
+
+def initialize(name, age)
+@name = name
+@age = age
+save
+end
+
+def save
+@@all << self
+end
+
+def self.all
+@@all
+end
+
+def new_meal(waiter, total, tip)
+  Meal.new(waiter, self, total, tip)
+end
+
+def meals
+Meal.all.select {|meal| meal.customer == self}
+# binding.pry
+end
+
+def waiters
+meals.collect {|meal| meal.waiter}
+end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
+  attr_accessor :name, :years
+  @@all = []
+
+def initialize(arg1, arg2)
+@name = name
+@years = years
+save
+end
+
+def save
+@@all << self
+end
+
+def self.all
+@@all
+end
+
+def new_meal(customer, total, tip)
+Meal.new(self, customer, total, tip)
+end
+
+def meals
+Meal.all.select {|meal| meal.waiter == self}
+end
+
+def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+
+  best_tipped_meal.customer
+end
 
 end
